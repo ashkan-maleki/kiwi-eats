32efa77bd34aff9b2d0ad4d849ad03216a147e48
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-03-16 20:34:19 +0330

    feat(auth): initialize gRPC server for AuthService
    - Create a main entry point to start the authentication gRPC server
    - Listen on port 50051 for incoming gRPC connections
    - Register AuthService with the gRPC server
    - Add logging for server startup and error handling

--- cmd/user-service/main.go
+++ cmd/user-service/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/ashkan-maleki/kiwi-eats/internal/user/pb"
+	"github.com/ashkan-maleki/kiwi-eats/internal/user/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	authService := &service.Auth{}
+
+	pb.RegisterAuthServiceServer(grpcServer, authService)
+
+	log.Println("User service is running on port 50051...")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
