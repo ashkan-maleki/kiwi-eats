bf8eeee4359747df0376e05796aafcd15095b296
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-03-16 20:11:00 +0330

    feat(auth): add Auth handler for authentication requests
    - Create Auth handler to handle authentication-related gRPC requests
    - Inject Auth Service into Auth Handler for request delegation
    - Implement Register method to forward calls to Auth Service
    - Prepare structure for future enhancements like logging & validation

---
+++ internal/user/handler/auth_handler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"context"
+	"github.com/ashkan-maleki/kiwi-eats/internal/user/pb"
+	"github.com/ashkan-maleki/kiwi-eats/internal/user/service"
+)
+
+type Auth struct {
+	authService *service.Auth
+}
+
+func NewAuth(authService *service.Auth) *Auth {
+	return &Auth{authService: authService}
+}
+
+func (h Auth) Register(ctx context.Context, request *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	return h.authService.Register(ctx, request)
+}
