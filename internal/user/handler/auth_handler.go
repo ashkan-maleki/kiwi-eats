57c4359d193d0ff167c39ec32452adfb1389723a
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-03-18 18:22:26 +0330

    feat(auth): implement Auth handler methods
    - Add `Login`, `ValidateToken`, `RefreshToken`, and `Logout` to `auth_handler.go`.
    - Delegate calls to corresponding AuthService methods.
    - Ensure gRPC request handling follows service-layer logic.

--- internal/user/handler/auth_handler.go
+++ internal/user/handler/auth_handler.go
@@ -18,3 +18,19 @@ func NewAuth(authService *service.Auth) *Auth {
 func (h Auth) Register(ctx context.Context, request *pb.RegisterRequest) (*pb.RegisterResponse, error) {
 	return h.authService.Register(ctx, request)
 }
+
+func (h Auth) Login(ctx context.Context, request *pb.LoginRequest) (*pb.LoginResponse, error) {
+	return h.authService.Login(ctx, request)
+}
+
+func (h Auth) ValidateToken(ctx context.Context, request *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	return h.authService.ValidateToken(ctx, request)
+}
+
+func (h Auth) RefreshToken(ctx context.Context, request *pb.RefreshTokenRequest) (*pb.RefreshTokenResponse, error) {
+	return h.authService.RefreshToken(ctx, request)
+}
+
+func (h Auth) Logout(ctx context.Context, request *pb.LogoutRequest) (*pb.LogoutResponse, error) {
+	return h.authService.Logout(ctx, request)
+}
